1a644b0d351ec55b3d1a4880b007cd93ac5733f4
SlamChillz <mendyslam@gmail.com>
2022-06-27 01:31:42 +0000

    Deciphering RSA algorithms via factorization

--- rsa.h
+++ rsa.h
@@ -6,6 +6,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <gmp.h>
 
+bool square(unsigned long long b);
+int factorise(unsigned long long n);
+int factorize(mpz_t n);
+void operate(char *str);
 
 #endif /* RSA */
